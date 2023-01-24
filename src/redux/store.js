7fdfd116c61675306d3fc318a0b549d21d87645a
Veronica <vveronikanika@gmail.com>
2023-01-24 13:54:47 +0200

    refactor: delete persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,21 +8,21 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
 import { configureStore } from '@reduxjs/toolkit';
 import { contactReducer } from './contactSlice';
 import { filterReducer } from './filterSlice';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+// };
 
-const persistedContactReducer = persistReducer(persistConfig, contactReducer);
+// const persistedContactReducer = persistReducer(persistConfig, contactReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactReducer,
+    contacts: contactReducer,
     filter: filterReducer,
   },
   middleware: getDefaultMiddleware =>
@@ -33,4 +33,4 @@ export const store = configureStore({
     }),
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
